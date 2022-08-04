d30bedae40ac40462709bb9c4d6026ca7fb1c038
yudantoardi <yudantoardi@gmail.com>
2022-08-04 21:54:06 +0700

    updates

--- asset/js/main.js
+++ asset/js/main.js
@@ -11,4 +11,23 @@ $(document).ready(function(){
     }
     });
 
+    $(".thumb").click(function(){
+        $(".tab-group").removeClass("show");
+        $($(this).attr("data-target")).addClass("show");
+    });
+
+    $(".points a").click(function(p){
+        p.preventDefault();
+        $(".dist-list").removeClass("show");
+        $($(this).attr("href")).addClass("show");
+    });
+
+    $(".burger-bt").click(function(bb){
+        bb.preventDefault();
+
+        $("header").toggleClass("show-mob-menu");
+        $("header").addClass("scrolled");
+        $(".burger-bt i").toggleClass("fa-bars fa-times");
+    });
+
 });
